d427d76aa4aa30e8085b2d4fac9052b23f179546
Dan Dumitriu <dandumitriu33@gmail.com>
2022-10-22 10:07:42 +0300

    Add constants for LOWER, UPPER and STEP

--- tempconvert/tempconvert.c
+++ tempconvert/tempconvert.c
@@ -3,6 +3,11 @@
 /*print Fahrenheit-Celsius table
 for f = 0, 20, ..., 300 */
 
+#define LOWER 0 /* lower limit of temperature table */
+#define UPPER 300 /* upper limit */
+#define STEP 20 /* step size */
+
+
 main()
 {
     int lower, upper, step;
@@ -17,4 +22,13 @@ main()
         printf("%4.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
+    printf("\n");
+    printf("----------------------\n");
+    printf("Solution using a FOR loop\n");
+    printf("----------------------\n");
+
+    /* int fahr; initialization not needed - it is done above */
+    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+        printf("%4.0f %6.1f\n", fahr, (5.0/9.0) * (fahr-32.0));
+
 }
